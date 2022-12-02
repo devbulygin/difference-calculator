ab0281f01a27b2254afdef7b1ea8184a3e7364d1
whoiswho68rus@gmail.com <githubRovnKresRim4>
2022-12-02 19:09:41 +0400

    fix checkstyle error

--- app/src/main/java/hexlet/code/DifferContent.java
+++ app/src/main/java/hexlet/code/DifferContent.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DifferContent {
+final class DifferContent {
 
     private Map<String, Object> map1 = new HashMap<>();
     private Map<String, Object> map2 = new HashMap<>();
@@ -16,31 +16,31 @@ public class DifferContent {
         return map1;
     }
 
-    public void setMap1(Map<String, Object> map1) {
-        this.map1 = map1;
+    public void setMap1(Map<String, Object> newMap1) {
+        this.map1 = newMap1;
     }
 
     public Map<String, Object> getMap2() {
         return map2;
     }
 
-    public void setMap2(Map<String, Object> map2) {
-        this.map2 = map2;
+    public void setMap2(Map<String, Object> newMap2) {
+        this.map2 = newMap2;
     }
 
     public Map<String, String> getDifferMap() {
         return differMap;
     }
 
-    public void setDifferMap(Map<String, String> differMap) {
-        this.differMap = differMap;
+    public void setDifferMap(Map<String, String> newDifferMap) {
+        this.differMap = newDifferMap;
     }
 
     public String getFormatName() {
         return formatName;
     }
 
-    public void setFormatName(String formatName) {
-        this.formatName = formatName;
+    public void setFormatName(String newFormatName) {
+        this.formatName = newFormatName;
     }
 }
